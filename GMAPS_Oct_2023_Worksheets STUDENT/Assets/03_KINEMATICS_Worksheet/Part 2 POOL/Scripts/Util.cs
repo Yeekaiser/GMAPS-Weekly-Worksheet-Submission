b186397e5d9930584c58a88acfe6c4fe3cf18df7
Yeekaiser <asura200576@gmail.com>
2023-12-07 22:03:51 +0800

    Completed worksheet except Reflection

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
@@ -1,12 +1,12 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Util
-// {
-//     public static float FindDistance(HVector2D p1, HVector2D p2)
-//     {
-//         return /*your code here*/;
-//     }
-// }
+public class Util
+{
+    public static float FindDistance(HVector2D p1, HVector2D p2)
+    {
+        return Mathf.Sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    }
+}
 
